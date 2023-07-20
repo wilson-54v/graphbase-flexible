1cc541d2c1d3f9cfd4fbdef66ecd63468d68cd0f
wilson-54v <wilsongachanja934@gmail.com>
2023-07-20 10:54:48 +0300

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import Image from 'next/image'
+
+
+const FooterColumn = () => (
+  <div className='footer_column'>
+    <h4 className='font-semibold'>Title</h4>
+    <ul className='flex flex-col gap-2 font-normal'>
+      Links
+    </ul>
+  </div>
+)
 
 const Footer = () => {
   return (
-    <div>
+    <footer className='flexStart footer'>
+      <div className="flex flex-col gap-12 w-full">
+        <div className="flex items-start flex-col">
+          <Image 
+             src="/logo-purple.svg"
+             width={115}
+             height={38}
+             alt="Flexibble"
+             />
+
+
+             <p className='text-start text-sm font-normal mt-5 max-w-xs'>
+             Flexibble is the world&apos;s leading community for creatives to share, grow, and get hired.
+             </p>
+        </div>
+        <div className='flex flex-wrap gap-12'>
+          <FooterColumn />
+
+        </div>
+      </div>
       Footer
-    </div>
+    </footer>
   )
 }
 
