10c874fafb7a77c362cf4f7818dd64e4fd593ae1
wilson-54v <wilsongachanja934@gmail.com>
2023-07-20 09:51:26 +0300

    update

---
+++ components/Navbar.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav className='flexBetween navbar'>
+      <div  className='flex-1 flexStart gap-10'>
+        <Link href="/">
+          <Image
+             src="/logo.svg"
+             width={115}
+             height={43}
+             alt="Flexibble"
+
+             />
+        </Link>
+        <ul className='xl:flex hidden text-small gap-7'>
+
+
+          
+        </ul>
+
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
